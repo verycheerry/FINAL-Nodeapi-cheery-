2dccca7648ed5b2078a4a2bfcd6c2fa66e0a3caf
verycheerry <chhavvesa@gmail.com>
2026-04-20 20:36:52 +0700

    Update index.js

--- NodeAPI/index.js
+++ NodeAPI/index.js
@@ -14,6 +14,6 @@ app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
 
-app.post('/order', (req, res) => {
+app.get('/order', (req, res) => {
     res.send('Order API working');
 });
